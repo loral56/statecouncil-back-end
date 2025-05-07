e5b0524a7987acd08207ed473886479a54a55922
Marwan Mohamed <lorallords@gmail.com>
2025-05-08 02:45:47 +0300

    Assign Port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/user.route");
 const path = require("path");
-
+const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +10,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/", userRoutes);
 
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("server is working");
 });
